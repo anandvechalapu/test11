725d91db6c38d2c38944e2618fd3428393c7e58f
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-08 12:53:54 +0530

    initial commit

---
+++ Forget password/Reset password 0.6415601462630451.java
@@ -0,0 +1,8 @@
+
+
+1. On your login page, add a “forgot password” link.
+2. When the user clicks the link, redirect them to a page that will prompt them to enter their email address or username.
+3. Once the user enters the email address or username, send out a password reset link to the email address.
+4. The password reset link should expire after a certain amount of time.
+5. When the user clicks the link, prompt them to enter a new password.
+6. Once the user enters the new password, store it in the system.
